ff1fd071f967bb4f9721bc0bcd0a2b820149e365
azrrael.exe <vcamargo.e@gmail.com>
2025-09-06 06:04:12 -0500

    feat: Add LLP type of output

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,36 @@
 #include <infrastructure/hal/LimitSwitchSensor.h>
 #include <app/catersian_move.h>
 #include <app/analog_sensor_reporter.h>
+#include <app/analog_sensor_reporter_llm.h>
 #include <infrastructure/hal/VoltageSensor_ADC.h>
+
 
 infrastructure::VoltageSensor_ADC voltageSensor(A0);
 app::VoltageSensorReporter voltageSensorReporter(voltageSensor);
+app::VoltageSensorReporterLlp voltageSensorReporterLlp(voltageSensor);
 
+bool output_llm = false;
+long timer = millis();
 
 void setup() {
+  pinMode(A1, INPUT);
   // initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
 }
 
 void loop() {
-  voltageSensorReporter.report(Serial);
-  delay(1000);
+  if (digitalRead(A1) == HIGH) {
+    output_llm = true;
+  } else {
+    output_llm = false;
+  }
+  if (millis() - timer > 1000) {
+    if (output_llm) {
+      voltageSensorReporterLlp.report(Serial);
+    } else {
+      voltageSensorReporter.report(Serial);
+    }
+    timer = millis();
+  }
 }
 
